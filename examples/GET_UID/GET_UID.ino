a491a46a77085b95612a2a32bf1117536234cad6
Martin Chlebovec <martinius96@gmail.com>
2022-07-28 19:49:41 +0200

    Update GET_UID.ino

--- examples/GET_UID/GET_UID.ino
+++ examples/GET_UID/GET_UID.ino
@@ -1,49 +1,40 @@
 /*|-------------------------------------------------|*/
-/*|Názov: Načítanie UID RFID kariet                 |*/
-/*|Popis: Načíta prostredníctvom RFID čítačky RC522 |*/
-/*|adresu RFID karty, ktorá je priložená a vypíše   |*/
-/*|UID na UART rozhranie.                           |*/
-/*|Zaznamenané UID kariet sa použijú v DOMINATOR-e  |*/
-/*|Autor: Martin Chlebovec                          |*/
+/*|Title: Reading RFID cards UID (unique identifier |*/
+/*|Compatible for Arduino Uno / Nano / ATmega328P   |*/
+/*|Author: Martin Chlebovec (martinius96@gmail.com) |*/
 /*|E-mail: martinius96@gmail.com                    |*/
-/*|Licencia pouzitia: MIT                           |*/
-/*|Revízia: 12. Marec 2021                          |*/
+/*|Revision: 28. July 2022                          |*/
 /*|-------------------------------------------------|*/
 
-//Wiring diagram: https://i.imgur.com/ROnQL9J.png (Compatible for Arduino Uno, Nano)
+
+//Template for writing RFID codes for RFID DOMINATION TIMER - Google SpreadSheet: https://docs.google.com/spreadsheets/d/1EHpdJi8v84eJ-6ExEPT6JuPkxI9Tfbm8v85zGRFvJ1Q/edit?usp=sharing
+
 #include <SPI.h>
 #include <MFRC522.h>
 #define SS_PIN 10
 #define RST_PIN 9
-
 MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
 void setup() {
   Serial.begin(9600);
-  while (!Serial);
+  Serial.println(F("Reading of RFID cards for RFID Domination Timer"));
   SPI.begin();
   rfid.PCD_Init(); // Init MFRC522
-  rfid.PCD_SetAntennaGain(112); // set to max (00001110)
-  rfid.PCD_DumpVersionToSerial();
+  rfid.PCD_SetAntennaGain(rfid.RxGain_max);
+  rfid.PCD_DumpVersionToSerial(); //Write version of Firmware to UART
 }
 
 void loop() {
   if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial()) {
-    Serial.print(F("Card UID: "));
     dump_byte_array(rfid.uid.uidByte, rfid.uid.size);
-    Serial.println();
-    Serial.print(F("PICC type: "));
-    MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
-    Serial.println(rfid.PICC_GetTypeName(piccType));
     rfid.PICC_HaltA();
     rfid.PCD_StopCrypto1();
   }
-
 }
 
 void dump_byte_array(byte *buffer, byte bufferSize) {
   for (byte i = 0; i < bufferSize; i++) {
   }
-  Serial.print("Code of CARD for DOMINATOR: ");
-  unsigned long kod = 10000 * buffer[4] + 1000 * buffer[3] + 100 * buffer[2] + 10 * buffer[1] + buffer[0]; //finalny kod karty
-  Serial.println(kod);
+  Serial.print(F("Detected UID (code) of RFID CARD: "));
+  unsigned long code = 10000 * buffer[4] + 1000 * buffer[3] + 100 * buffer[2] + 10 * buffer[1] + buffer[0]; //finalny kod karty
+  Serial.println(code);
 }
